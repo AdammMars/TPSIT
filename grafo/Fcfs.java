36cd69263e050826bcccac293c5d0506478657aa
AdammMars <119665697+AdammMars@users.noreply.github.com>
2023-11-22 13:20:48 +0100

    Add files via upload

---
+++ grafo/Fcfs.java
@@ -0,0 +1,46 @@
+package it.edu.iisgubbio.grafo;
+
+public class Fcfs {
+
+	String processo[];
+	int servizio[];
+	int arrivo[];
+	int attesa[];
+	int esecuzione[];
+
+	public Fcfs(int numeroprocessi, int tempoEsecuzione) {
+
+		String[] numeroProcesso = processo;
+
+		for (int c = 0; c < this.processo.length - 1; c++) {
+			if (numeroProcesso.equals(this.processo[c])) {
+
+				esecuzione[c] = tempoEsecuzione;
+
+			}
+
+		}
+
+	}
+
+	public void setEsecuzione() {
+
+		
+		
+		
+	}
+
+	
+	
+	
+	
+	void servizioTotale() {
+
+		
+	}
+
+	public void getEsecizione() {
+
+	}
+
+}
